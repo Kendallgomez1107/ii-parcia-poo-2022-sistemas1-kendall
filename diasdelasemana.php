30473163327ca0110be49f4007a602919c33123f
Kendall Gomez <95137012+Kendallgomez1107@users.noreply.github.com>
2022-01-29 19:43:32 -0600

    Add files via upload

---
+++ diasdelasemana.php
@@ -0,0 +1,18 @@
+
+<html lang="en">
+<head>
+    <title>dias de las semana </title>
+</head>
+<body>
+    <?php
+    $dia=array ("lunes","martes","miercoles","jueves","viernes","sabado","domingo");
+  
+    for ($i=0; $i<=6; $i++) { 
+    
+    echo   $dia[$i]."<br>";
+}
+   
+   
+    ?>
+</body>
+</html>
